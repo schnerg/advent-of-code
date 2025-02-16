c05ec02b49932043501fd40fb96bd83c96163cd2
schnerg <schnerg293@gmail.com>
2025-02-15 17:18:23 -0800

    day1

---
+++ day_1/p1/src/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/main.h"
+
+typedef struct
+{
+	int * left;
+	int * right;
+	int count;
+}lists;
+
+lists l;
+
+void append_to_list( int a, int b )
+{
+	l.count++;
+	l.right = realloc( l.right , l.count * sizeof( int ) );
+	l.left  = realloc( l.left , l.count * sizeof( int ) );
+	l.left[l.count-1] = a;
+	l.right[l.count-1] = b;
+	return;
+}
+
+void parse_file( const char * file_name )
+{
+	FILE * fp = fopen( file_name, "r" );
+	int n1, n2, i = 0, count = 1;
+	char c;
+	char * buf = calloc( count, sizeof( char ) );
+	while( ( c = fgetc( fp ) ) != EOF )
+	{
+		switch( c )
+		{
+			case '\n':
+			{
+				buf[i] = '\0';
+				sscanf( buf, "%i%i", &n1, &n2 );
+				append_to_list( n1, n2 );
+				i = 0;
+			}break;
+			
+			default:
+			{
+				buf[i++] = c;
+				if( i >= count )	
+					buf = realloc( buf, ++count * sizeof( char ) );
+			}break;
+		
+		}
+	}
+	fclose(fp);
+	return;
+}
+//right?
+int comp( const void * a, const void * b )
+{
+	return (*(int*)a - *(int*)b);
+}
+void sort_lists( void )
+{
+	qsort(l.left, l.count, sizeof(int), comp);	
+	qsort(l.right, l.count, sizeof(int), comp);	
+	return;
+}
+
+int diff( int a, int b )
+{
+	int i = ( a > b )? a - b: b - a;
+	return i;
+}
+
+
+int find_diff()
+{
+	int result = 0;
+	for( int i = 0; i < l.count; i++ )
+	{
+		result += diff( l.left[i], l.right[i] );
+	}
+	return result;
+}
+
+
+void free_lists( void )
+{
+	free( l.right );
+	free( l.left );
+	return;
+}
+
+
+int main(int argc, char * argv[])
+{
+	l.count = 0;
+	parse_file( "input.txt" );
+	sort_lists();
+	int result = find_diff();
+	free_lists();
+	printf( "%i\n", result );
+	return 0;
+}
